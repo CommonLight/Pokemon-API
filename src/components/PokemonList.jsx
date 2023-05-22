9f5de7055e79b261df922dd129b17d87d7068662
Luke Erickson <mom2seth09@hotmail.com>
2023-05-22 11:34:29 -0500

    Linked up the API and finished styling the page

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 
-const PokemonList = () => {
-  return (
-    <div id='wrapper'>
-        <img className='pokeImage' src={ require("./imgs/Poke.png") } alt="pokemon logo" />
-        <p className="pokeTitle">THE COMPLETE LIST</p>
-        <hr />
-        <ul className='pokeList'>
-            <li>Pikachu</li>
-            <li>Charmander</li>
-            <li>Squirtle</li>
-        </ul>
-    </div>
-  )
+const PokemonList = (props) => {
+    const [pokemon, setPokemon] = useState([])
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon-species?limit=1010')
+            .then(response => response.json())
+            .then(response => setPokemon(response.results))
+    }, [])
+
+    return (
+        <div id='wrapper'>
+            <img className='pokeImage' src={ require("./imgs/Poke.png") } alt="pokemon logo" />
+            <p className="pokeTitle">THE COMPLETE LIST</p>
+            <hr />
+            <ul className='pokeList'>
+            {pokemon.length > 0 && pokemon.map((pokemon, index) => {
+                return (<li key= {index} > {pokemon.name} </li>)
+            })}
+            <p className='end'>THE END</p>
+            </ul>
+        </div>
+    )
 }
 
 export default PokemonList
